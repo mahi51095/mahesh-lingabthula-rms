ccc3bf6b4e9454ca8973eefb4b3853039decfc63
mahi51095 <32168119+mahi51095@users.noreply.github.com>
2017-09-25 20:29:51 +0530

    Add files via upload
    items class of restaurant management system

---
+++ items.java
@@ -0,0 +1,17 @@
+
+public class items {
+	private String name;
+	private String category;
+	private double price;
+	private int size;
+	public items(String name,String category,double price,int size){
+		this.name=name;
+		this.category=category;
+		this.price=price;
+		this.size=size;
+		
+	}
+	
+	
+
+}
