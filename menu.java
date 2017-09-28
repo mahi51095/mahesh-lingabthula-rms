3e2cfc267d751d9d1cdce94e972eabf55ad6b292
mahi51095 <32168119+mahi51095@users.noreply.github.com>
2017-09-28 19:06:54 +0530

    Add files via upload
    menu class has been updated.

--- menu.java
+++ menu.java
@@ -2,48 +2,59 @@
 public class menu {
 	
 	//fields
-	 private ArrayList<items> Starters=new ArrayList<items>();
-	 private ArrayList<items> maincourse=new ArrayList<items>();
-	 private ArrayList<items> beverages=new ArrayList<items>();
-	 private ArrayList<items> desserts=new ArrayList<items>();
-	 private ArrayList<items> icecream=new ArrayList<items>();
+	 private ArrayList<Item> Starters=new ArrayList<Item>();
+	 private ArrayList<Item> maincourse=new ArrayList<Item>();
+	 private ArrayList<Item> beverages=new ArrayList<Item>();
+	 private ArrayList<Item> desserts=new ArrayList<Item>();
+	 private ArrayList<Item> icecream=new ArrayList<Item>();
 	 
 	 // constructors
 	 public menu(){
 		 populateMenu();
 	 }
-		 
-	 }
-	
+	  // return a specific starters
+	  public Item getStarters(int index){
+		  return Starters.get(index);
+	  }
+	  // returns the size of the starters
+	  public int getStartersSize(){
+		  return Starters.size();
+	  }
+	  
+	  
+	  // returns all starters as a string
+	  
+	 
+	//methods
 	 public void populateMenu(){
 		 //Starters
-		Starters.add(new items("chicken manuchuria","starters", 60));
-		Starters.add(new items("chicken 65" " Starters",70));
-		Starters.add(new items("veg maunchuria"," Starters", 45));
+		Starters.add(new Item("chicken manuchuria \t\t","starters", 60));
+		Starters.add(new Item("chicken 65 \t\t ", " Starters",70));
+		Starters.add(new Item("veg maunchuria \t\t"," Starters", 45));
 		
 		//main course
-		maincourse.add(new items("chicken biryani single","maincourse",100));
-		maincourse.add(new items("chicken biryani family","maincourse",300));
-		maincourse.add(new items("Mutton biryani single","maincourse",130));
-		maincourse.add(new items("Mutton biryani family","maincourse",350));
+		maincourse.add(new Item("chicken biryani single \t\t","maincourse",100));
+		maincourse.add(new Item("chicken biryani family \t\t","maincourse",300));
+		maincourse.add(new Item("Mutton biryani single \t\t","maincourse",130));
+		maincourse.add(new Item("Mutton biryani family \t\t","maincourse",350));
 		
 		//beverages
-		beverages.add(new items("pepsi","beverages",25));
-		beverages.add(new items("coca cola","beverages",25));
-		beverages.add(new items("Thumps up","beverages",25));
-		beverages.add(new items("sprite","beverages",25));
+		beverages.add(new Item("pepsi \t\t","beverages",25));
+		beverages.add(new Item("coca cola \t\t","beverages",25));
+		beverages.add(new Item("Thumps up \t\t","beverages",25));
+		beverages.add(new Item("sprite \t\t","beverages",25));
 		
 		//desserts
-		desserts.add(new items("qurbani ka meta","desserts",40));
-		desserts.add(new items("dauble ka meta","desserts",40));
+		desserts.add(new Item("qurbani ka meta \t\t","desserts",40));
+		desserts.add(new Item("dauble ka meta \t\t","desserts",40));
 		
 		//ice creams
-		icecream.add(new items("butter scotch","icecream",30));
-		icecream.add(new items("vennila","icecream",30));
+		icecream.add(new Item("butter scotch \t\t","icecream",30));
+		icecream.add(new Item("vennila \t\t","icecream",30));
 		
 		
 	 
 		  
-	
+	 }
 
 }
